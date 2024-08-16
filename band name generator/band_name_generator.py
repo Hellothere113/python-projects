10e21f26f803046522ab4ec1acfa2313dce77958
Hellothere113 <arq2003oma@gmail.com>
2024-08-16 22:34:00 +0300

    this is my first step in to the big world of pythonity xd

---
+++ band name generator/band_name_generator.py
@@ -0,0 +1,5 @@
+print("hello there!")
+city_name = input("where did you grow up?\n")
+pet_name = input("what pet do you have?\n")
+Band_name = city_name + pet_name
+print("so your band name  is:"+ Band_name)
